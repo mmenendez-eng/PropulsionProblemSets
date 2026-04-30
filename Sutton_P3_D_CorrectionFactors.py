d279c61aa46e4be63c4aecfe6f5ac4bcd6634275
mmenendez-eng <malejandrom09@gmail.com>
2026-04-30 13:52:41 -0700

    Changed to metric, additional documentation

--- Sutton_P3_D_CorrectionFactors.py
+++ Sutton_P3_D_CorrectionFactors.py
@@ -14,33 +14,38 @@ a) calculate the c*-efficiency for this thruster.
 Also, if ζCF = 0.94, 
 b) calculate an "effective exhaust velocity correction factor" (ζc).
 """
+
 ## IMPORTS
 import numpy as np
 
 ## GIVEN/CONSTANTS
-actual_T1 = 6200 # deg. F
-actual_T1 = actual_T1 + 459.67 # convert to deg. R
-actual_P1 = 1000 # psia
-k = 1.24
-Mol = 23.3 # lbm/lb-mol
-g0 = 32.2 # ft/s^2
-R_universal = 1544 # universal gas constant, ft-lbf/lb-mol-R
-actual_cstar = 1680*3.281  # experimental value, ft/s
-zeta_CF = 0.94
+actual_T1 = 6200    # Chamber temperature, deg. F
+actual_P1 = 1000    # Chamber pressure, psia
+k = 1.24            # Specific Heat Ratio
+Mol = 23.3          # Molecular mass, kg/kmol
+R_universal = 8314  # Universal gas constant, J/(kmol-K)
+actual_cstar = 1680 # Experimental c*, m/s
+zeta_CF = 0.94      # Thrust Coefficient correction factor
 
 ## FUNCTIONS
 def cstar(k,R,T):
-    return np.sqrt(k*R*T)/(k*np.sqrt((2/(k+1))**((k+1)/(k-1))))
+    return np.sqrt(k*R*T)/(k*np.sqrt((2/(k+1))**((k+1)/(k-1)))) # m/s
+
+def fahrenheit_to_kelvin(T_F):
+    return (T_F-32)*5/9 + 273.15
 
 ## ANALYSIS
+# Convert temperature from deg. F to Kelvin
+T1_K = fahrenheit_to_kelvin(actual_T1) # K
+
 # a) Calculate c*-efficiency
-R_specific = R_universal/(Mol*g0)
-ideal_cstar = cstar(k,R_specific,actual_T1) # m/s
+R_specific = R_universal/(Mol)
+ideal_cstar = cstar(k,R_specific,T1_K) # m/s
 zeta_cstar = actual_cstar/ideal_cstar
 # b) Calculate effective exhuast velocity correction factor
 zeta_velocity = zeta_cstar*zeta_CF
 
 ## OUTPUT
-print(f'Ideal c*-efficiency = {ideal_cstar:.3f} ft/s')
-print(f'Ideal c*-efficiency = {ideal_cstar/3.281:.3f} m/s')
+print(f'Ideal c* = {ideal_cstar:.0f} m/s')
+print(f'c*-efficiency = {zeta_cstar:.3f}')
 print(f'Effective Exhaust Velocity Correction Factor = {zeta_velocity:.3f}')
