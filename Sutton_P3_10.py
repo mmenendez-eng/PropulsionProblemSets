6b229408e75e1ae93b89d05a5736b522707273a3
mmenendez-eng <malejandrom09@gmail.com>
2026-05-29 10:39:11 -0700

    Add files via upload

--- Sutton_P3_10.py
+++ Sutton_P3_10.py
@@ -7,13 +7,13 @@ import numpy as np
 from scipy.optimize import fsolve
 
 ## GIVEN/CONSTANTS
-P_c = 2.068e6           # Pa
-T_c = 2222              # K
-mdot = 1.0              # kg/s
+P_c = 2.068e6           # Pa, Chamber Pressure
+T_c = 2222              # K, Chamber Temperature
+mdot = 1.0              # kg/s, Mass Flow Rate
 k = 1.30
-R = 345.7               # J/kg.K
-g0 = 9.81                # m/s^2
-P_amb = 101.325e3       # Pascals
+R = 345.7               # J/kg.K, Specific Gas Constant
+g0 = 9.81               # m/s^2, Standard Gravity
+P_amb = 101.325e3       # Pascals, Ambient Pressure
 ExpR = 2000             # Nozzle Expansion Ratio, Ae/At
 
 ## FUNCTIONS
@@ -26,27 +26,25 @@ def calculate_exit_mach_number(area_ratio, k):
     M_exit = fsolve(area_mach_relation, 8)[0]  # Initial guess for supersonic flow
     return M_exit
 
-def ideal_max_exhaust_velocity(k,R,Tc):
-    return np.sqrt(2*k*R*T_c/(k-1))
+def ideal_max_exhaust_velocity(k, R, Tc):
+    return np.sqrt(2 * k * R * Tc / (k - 1))
 
-def exhaust_velocity(k,Tc,R,P1,P2):
+def exhaust_velocity(k, Tc, R, P1, P2):
     """Calculates exhaust velocity based on pressure ratio and chamber conditions"""
-    return np.sqrt(2*k/(k-1)*R*Tc*(1-(P2/P1)**((k-1)/k)))
+    return np.sqrt(2 * k / (k - 1) * R * Tc * (1 - (P2 / P1)**((k - 1) / k)))
 
-def real_max_exhaust_velocity(k,R,Pc,Tc,Pe,Te,eps):
-    return Pe/(R*Te)*np.sqrt(k*R*Tc)/(eps*Pc*((k+1)/2)**(1/(k-1)))
 
 ## ANALYSIS
 # Maximum Exhaust Velocity:
 # - at Vacuum (ExpR -> Inf)
-v_max = ideal_max_exhaust_velocity(k,R,T_c)
+v_max = ideal_max_exhaust_velocity(k, R, T_c) # m/s
 
 # - at given Expansion Ratio (ExpR = 2000)
-Me = calculate_exit_mach_number(ExpR,k)
-T_e = T_c/(1 + (k-1)/2*Me**2)
-v_max_real = Me*np.sqrt(k*R*T_e)
+Me = calculate_exit_mach_number(ExpR, k)     # Exit Mach No.
+T_e = T_c / (1 + (k - 1) / 2 * Me**2)               # Kelvin
+v_max_real = Me * np.sqrt(k * R * T_e)            # m/s
 
 ## OUTPUT
 print('\nRESULTS')
 print(f'Maximum Exhaust Velocity at Vacuum       (ExpR -> Inf) = {v_max:.2f} m/s')
-print(f'Maximum Exhaust Velocity at Design Point (ExpR = 2000) = {v_max_real:7.2f} m/s')
+print(f'Maximum Exhaust Velocity at Design Point (ExpR = 2000) = {v_max_real:.2f} m/s')
